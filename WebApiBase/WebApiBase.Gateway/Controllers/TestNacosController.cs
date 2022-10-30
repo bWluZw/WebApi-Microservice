ab175f120e5987878f1e9efbb420229b1d0eb4d8
ming62951413@outlook.com <ming62951413@outlook.com>
2022-10-30 23:20:01 +0800

    未完成：测试网关

---
+++ WebApiBase/WebApiBase.Gateway/Controllers/TestNacosController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApiBase.Gateway.Controllers
+{
+    public class TestNacosController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
